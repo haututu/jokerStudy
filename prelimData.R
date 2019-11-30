def89b41c6ad3e9ec23d9c898a97c6659942f124
Haututu <taylor.winter@vuw.ac.nz>
2019-11-30 22:09:06 +1300

    Testing people with overly fast responses.

--- prelimData.R
+++ prelimData.R
@@ -40,3 +40,35 @@ dat %>%
   group_by(`Cell Number`) %>%
   summarise(n = n()) %>%
   filter(n > 1)
+
+dat %>%
+  select(Gender,
+         DOB,
+         `Cell Number`,
+         `Duration (in seconds)`) %>%
+  filter(row_number() != 1) %>%
+  mutate(DOB_date = as.Date(DOB, "%d/%m/%Y"),
+         Gender = as.factor(Gender),
+         Age = as.numeric(difftime(as.Date("2019-11-29"), DOB_date)) / 365,
+         Duration = as.numeric(`Duration (in seconds)`) / 60
+  ) %>%
+  group_by(`Cell Number`) %>%
+  summarise(n = n()) %>%
+  filter(n > 1)
+
+dat %>%
+  select(Gender,
+         DOB,
+         `Cell Number`,
+         `Duration (in seconds)`) %>%
+  filter(row_number() != 1) %>%
+  mutate(DOB_date = as.Date(DOB, "%d/%m/%Y"),
+         Gender = as.factor(Gender),
+         Age = as.numeric(difftime(as.Date("2019-11-29"), DOB_date)) / 365,
+         Duration = as.numeric(`Duration (in seconds)`) / 60
+  ) %>%
+  #filter(Duration < 8 & !(is.na(`Cell Number`))) %>%
+  filter(Duration < 60 & !(is.na(`Cell Number`))) %>%
+  ggplot(aes(x=Duration)) +
+  geom_density() +
+  theme_classic()
