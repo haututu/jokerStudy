a8c493cbe178cccc7fcae6061f4747d219607a9c
Haututu <taylor.winter@vuw.ac.nz>
2019-12-11 12:49:13 +1300

    Changed naming scheme for the data from SPSS.

--- cleaning/makeFactors.R
+++ cleaning/makeFactors.R
@@ -1,15 +1,17 @@
 ## A list of functions to take in a dataset and give back an aggregated factor
 library(lavaan)
 
-dat <- readxl::read_xlsx("data/Media+and+Mental+Illness_+Questionnaire+One_November+29,+2019_11.30.xlsx")
-colnames(dat) <- gsub(" ", "", colnames(dat))
+dat <- haven::read_sav("data/JokerJustTimeCCprejudicescale1ds5.sav")
+
+dat_old <- readxl::read_xlsx("data/Media+and+Mental+Illness_+Questionnaire+One_November+29,+2019_11.30.xlsx") %>%
+  
 
 ######## Prejudice
 
-fearAvoid <- paste("fearAvoid =~", paste0("Prejudice1_", 1:8, collapse=" + "))
-malevolence <- paste("malevolence =~", paste0("Prejudice1_", 9:14, collapse=" + "), "+ Prejudice2_1", "+Prejudice2_2")
-authority <- paste("authority =~", paste0("Prejudice2_", 3:8, collapse=" + "))
-unpredict <- paste("unpredict =~", paste0("Prejudice2_", 9:14, collapse=" + "))
+fearAvoid <- paste("fearAvoid =~", paste0("Prejudice_", 1:8, collapse=" + "))
+malevolence <- paste("malevolence =~", paste0("Prejudice_", 9:16, collapse=" + "))
+authority <- paste("authority =~", paste0("Prejudice_", 17:22, collapse=" + "))
+unpredict <- paste("unpredict =~", paste0("Prejudice_", 23:28, collapse=" + "))
 
 prejudice <- cfa(
   paste(fearAvoid,
@@ -22,4 +24,4 @@ prejudice <- cfa(
 
 summary(prejudice)
 
-semPlot::semPaths(prejudice, "est", residuals=FALSE, rotation=4, structural=TRUE, edge.label.cex = 1.2, curvature = 5)
+semPlot::semPaths(prejudice, "std", residuals=FALSE, rotation=4, structural=TRUE, edge.label.cex = 1.2, curvature = 5)
