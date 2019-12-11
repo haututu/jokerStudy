5d5ca266429dc6bc1745c66205dec85b6fa5a51c
Taylor <taylor.winter@vuw.ac.nz>
2019-12-11 22:55:15 +1300

    Sorting load to play with CFA at different timepoints.
    Making columns lowercase.

--- cleaning/makeFactors.R
+++ cleaning/makeFactors.R
@@ -1,17 +1,19 @@
 ## A list of functions to take in a dataset and give back an aggregated factor
+library(tidyverse)
 library(lavaan)
 
-dat <- haven::read_sav("data/JokerJustTimeCCprejudicescale1ds5.sav")
-
-dat_old <- readxl::read_xlsx("data/Media+and+Mental+Illness_+Questionnaire+One_November+29,+2019_11.30.xlsx") %>%
-  
+readxl::read_xlsx("data/Media-and-Mental-Ilness-Joker-Terminator-FINAL-ds2.xlsx", "FINAL") %>%
+  rename_all(tolower) %>%
+  filter(!(is.na(number))) %>%
+  group_by(number) %>%
+  filter(n() > 2)
 
 ######## Prejudice
 
-fearAvoid <- paste("fearAvoid =~", paste0("Prejudice_", 1:8, collapse=" + "))
-malevolence <- paste("malevolence =~", paste0("Prejudice_", 9:16, collapse=" + "))
-authority <- paste("authority =~", paste0("Prejudice_", 17:22, collapse=" + "))
-unpredict <- paste("unpredict =~", paste0("Prejudice_", 23:28, collapse=" + "))
+fearAvoid <- paste("fearAvoid =~", paste0("prejudice_", 1:8, collapse=" + "))
+malevolence <- paste("malevolence =~", paste0("prejudice_", 9:16, collapse=" + "))
+authority <- paste("authority =~", paste0("prejudice_", 17:22, collapse=" + "))
+unpredict <- paste("unpredict =~", paste0("prejudice_", 23:28, collapse=" + "))
 
 prejudice <- cfa(
   paste(fearAvoid,
@@ -25,3 +27,4 @@ prejudice <- cfa(
 summary(prejudice)
 
 semPlot::semPaths(prejudice, "std", residuals=FALSE, rotation=4, structural=TRUE, edge.label.cex = 1.2, curvature = 5)
+
