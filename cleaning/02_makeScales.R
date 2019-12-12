f74470f395a35666cb018a76c46deb9e41167bad
Haututu <taylor.winter@vuw.ac.nz>
2019-12-12 13:30:44 +1300

    Added recoding for prejudice and some cleaning steps.

--- cleaning/02_makeScales.R
+++ cleaning/02_makeScales.R
@@ -1,5 +1,21 @@
 source("cleaning/01_load.R")
 
+####### Recode questions
+
+recodeInt <- function(x, upper) {
+  upper - x
+}
+
+dat <- dat %>%
+  mutate_at(vars(num_range("prejudice_", c(5:8, 13:16, 20:22, 26:28))), funs(recodeInt(., 7)))
+
 ####### Prejudice
+
+dat <- dat %>%
+  mutate(prejudice = rowSums(select(., contains("prejudice_")))
+         ) %>%
+  group_by(number) %>%
+  mutate(prejudiceLag = lag(prejudice)) %>%
+  ungroup()
 
 # Reverse coding
