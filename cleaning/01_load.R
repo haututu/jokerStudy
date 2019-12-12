d6eb015917797ea93329aca427e577797f15d519
Haututu <taylor.winter@vuw.ac.nz>
2019-12-12 13:30:44 +1300

    Added recoding for prejudice and some cleaning steps.

--- cleaning/01_load.R
+++ cleaning/01_load.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 
-dat <- readxl::read_xlsx("data/Taylor-Joker-Study.xlsx") %>%
+dat <- readxl::read_excel("data/Taylor-Joker-Study-T123-ds2-cleaned.xlsx") %>%
   rename_all(tolower) %>% # Make lower case
   filter(!(is.na(number))) %>% # Remove NA cellnumbers
   group_by(number) %>%
-  filter(n() == 2) %>% # Remove anyone who didnt complete both surveys
-  mutate(movie = max(movie, na.rm=TRUE)) # Add movie condition to T1
+  mutate(movie = max(movie, na.rm=TRUE), # Add movie condition to T1
+         time = ifelse(time == 3, time + day - 1, time) # Correct T3 variable if they responded a day late or more
+         ) %>% 
+  ungroup()
