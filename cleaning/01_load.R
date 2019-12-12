dca6cb3501f6a792e253f5cc939600b195a3fadc
Taylor <taylor.winter@vuw.ac.nz>
2019-12-12 17:49:02 +1300

    Swapping to use latent variables in regression.

--- cleaning/01_load.R
+++ cleaning/01_load.R
@@ -4,7 +4,13 @@ dat <- readxl::read_excel("data/Taylor-Joker-Study-T123-ds2-cleaned.xlsx") %>%
   rename_all(tolower) %>% # Make lower case
   filter(!(is.na(number))) %>% # Remove NA cellnumbers
   group_by(number) %>%
-  mutate(movie = max(movie, na.rm=TRUE), # Add movie condition to T1
+  mutate(movie = max(movie, na.rm=TRUE),
+         age = max(age, na.rm=TRUE),
+         gender = max(gender, na.rm=TRUE), # Add movie condition to T1
          time = ifelse(time == 3, time + day - 1, time) # Correct T3 variable if they responded a day late or more
-         ) %>% 
-  ungroup()
+         ) %>%
+  ungroup() %>% 
+  mutate(movie = as.factor(movie),
+         gender = as.factor(gender),
+         number = as.factor(number)
+         )
